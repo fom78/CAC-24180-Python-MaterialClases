5df0b9e9e4957c68d4c066e0328ff70079846a55
Fernando Masino <fernando.masino@bue.edu.ar>
2024-04-17 22:03:27 -0300

    clase 13

---
+++ clases/clase_13-JS-Introduccion_a_Javascript/javascript01/inicioenjavascript.js
@@ -0,0 +1,84 @@
+// alert("Holaaaaaa!!!")
+
+console.log("Holaaaaaa!!!")
+
+// console.error("Advertencia!!!!!!")
+
+
+document.write("Hola campeones!!!!")
+
+
+
+// COMENTARIOS
+
+/*
+
+Comentario en bloque
+Comentario en bloque
+
+
+*/
+
+// VARIABLES
+
+let var1
+
+
+var1 = -23
+
+document.write(var1)
+// typeof("Fernando")
+document.write(typeof("Fernando"))
+
+var1 = 59.6
+
+
+var2 = "Fernando"
+
+// CONCATENACION
+var2 = var2 + " Masino"
+
+document.write(var2)
+document.write(typeof(var1))
+
+
+let x, y, res
+
+x = 2
+y=9
+res = y + x
+
+console.log(res)
+
+console.log()
+
+
+b = 34
+var b1 = 34
+
+let b2 = 34
+
+{
+    let b3 = 9
+    console.log(b3);
+}
+
+// console.log(b3);
+
+const PI = 3.1415
+console.log(PI)
+
+constante1 = 35
+
+// console.log(constante1)
+
+let movie
+
+movie = "Harry potter"
+
+const quantityTrendingMovies = 10
+console.log(movie)
+
+
+
+
---
+++ clases/clase_13-JS-Introduccion_a_Javascript/javascript01/js02.js
@@ -0,0 +1,30 @@
+
+
+let isAllowed = true
+
+
+
+isAllowed = false
+
+// isAllowed = "Nose"
+
+
+console.log(">>>>>",isAllowed, typeof(isAllowed))
+
+// Operadores relacionales
+
+let resultado
+
+// Expresion
+// resultado = operando1 operador operando2
+resultado = 5 > 5
+
+resultado = "5" === 5
+
+const numerito = "5"
+resultado = numerito === 5
+
+
+
+
+console.log("➡️",resultado);

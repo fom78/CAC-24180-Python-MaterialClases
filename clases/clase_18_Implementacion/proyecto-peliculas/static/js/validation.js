c0a66f6ab7c82c3b61fae04fbadd21ab5cd5864b
Fernando Masino <fernando.masino@bue.edu.ar>
2024-05-08 20:28:21 -0300

    18 dictada

--- clases/clase_18_Implementacion/proyecto-peliculas/static/js/validation.js
+++ clases/clase_18_Implementacion/proyecto-peliculas/static/js/validation.js
@@ -2,9 +2,9 @@
 // Haremos algunas validaciones, pero antes practicaremos
 
 // Agregar un texto (2024) al lado  del corazon ❤️
+const h2Anio = document.querySelector('h2') 
 
-
-
+h2Anio.insertAdjacentText('beforeend','2024')
 
 /*
 - Implementar una funcion para validar el formulario
@@ -14,5 +14,29 @@
 
 */ 
 
+const validarFormulario= (evento) => {
+    evento.preventDefault()
+    const primerNombre = document.getElementById("firstname")
+    const divErrorPrimerNombre = document.querySelector("#error-firstname")
+    divErrorPrimerNombre.innerHTML = ""
+
+    if (primerNombre.value === "") {
+        
+        divErrorPrimerNombre.insertAdjacentText("afterbegin","El nombre no puede ser vacio!!!")
+    }
+
+    // password
+    const clave = document.getElementById("password")
+    const divErrorClave = document.querySelector("#error-password")
+    divErrorClave.innerHTML = ""
+    
+    if (clave.value.length < 6) {
+        
+        divErrorClave.insertAdjacentText("afterbegin","La pass debe superar los 6 caracteres ")
+    }
+}
 
 // agregar el listener al formulario
+
+const formularioRegistro = document.querySelector("#formRegister")
+formularioRegistro.addEventListener('submit',validarFormulario)

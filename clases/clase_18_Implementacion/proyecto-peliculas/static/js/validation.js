df335650b5370b7743f020091a4e700d874384a3
Fernando Masino <fernando.masino@bue.edu.ar>
2024-05-08 17:41:58 -0300

    clase 18

---
+++ clases/clase_18_Implementacion/proyecto-peliculas/static/js/validation.js
@@ -0,0 +1,18 @@
+
+// Haremos algunas validaciones, pero antes practicaremos
+
+// Agregar un texto (2024) al lado  del corazon ❤️
+
+
+
+
+/*
+- Implementar una funcion para validar el formulario
+- validar el nombre no vacio
+- validar al enviar
+- Dar un feedback del error
+
+*/ 
+
+
+// agregar el listener al formulario
